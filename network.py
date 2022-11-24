080a8371bb88ac6d396cc09e8ca0cbcb9654b889
Terasuki <xt.wanglin@gmail.com>
2022-11-24 12:40:38 +0000

    add units, variable velocity

--- network.py
+++ network.py
@@ -1,5 +1,5 @@
 """
-Week 5
+Week 8
 
 Ray Hu & Xietao
 """
@@ -11,9 +11,9 @@ import random
 """
 Parameters.
 numberOfRods: the number of rods, also the number of motors.
-length: the length of the rods, enter a matrix of size equal to the number of rods.
-velocity_d: diffusion velocity.
-velocity_p: persistence velocity.
+length: the length of the rods, enter a matrix of size equal to the number of rods. (in nm)
+velocity_d: diffusion velocity. (in nm/ns)
+velocity_p: persistence velocity. (in nm/ns)
 seed: seed used to generate motor position.
 makeAnimation: set True if you want simulation animated, set False to skip.
 repeatedSystems: set True for multiple simulations with different seed.
@@ -24,29 +24,29 @@ diffSampling: 0 is constant, 1 for uniformly distribution, 2 Gaussian.
 persSample: 0 is constant, 1 for uniformly distribution, 2 Gaussian.
 allowMotorCrossing: set True to allow motor crossing.
 finalTime: number of timesteps to be considered.
-recordTime: number of timesteps before each recording.
+recordTime: number of timesteps before each recording. (in ns)
 motors: motors initial condition w.r.t the rods, enter the full matrix.
 """
 
-numberOfRods = 23
-length = np.ones(numberOfRods)
-velocity_d = 0.01
-velocity_p = 0.01
+numberOfRods = 201
+length = np.ones(numberOfRods)*1000
+velocity_d = 10
+velocity_p = 1
 seed = 1
-makeAnimation = True
+makeAnimation = False
 repeatedSystems = False
-numberOfRepetitions = 2000
+numberOfRepetitions = 50
 # Don't change these
 np.random.seed(seed)
 random.seed(seed)
 # ----
 seqUpdate = 2
-diffSampling = 0
-persSample = 0
+diffSampling = 1
+persSample = 1
 allowMotorCrossing = True
-finalTime = 400
+finalTime = 8000
 recordTime = 2
-motors = np.random.rand(numberOfRods, 2)
+motors = np.random.rand(numberOfRods, 2)*1000
 
 def RodsArray(Rposition, Mposition, length, numberOfRods):
 
@@ -71,14 +71,20 @@ def randwalk(numberofsteps, Rposition, Mposition, numberOfRods, pervelo, diffvel
             order = random.choices(order, k=numberOfRods)
         for i in order: # for each motor
             dice1 = np.random.uniform(0,1)
+            v_p = pervelo
+            v_d = diffvelo
             if diffSampling == 0:
                 pass
             elif diffSampling == 1:
-                v_d *= random.uniform(0,1)
+                v_d *= random.uniform(0,1)*2
+            elif diffSampling == 2:
+                v_d = abs(np.random.normal(0, (v_d)))
             if persSample == 0:
                 pass
             elif persSample == 1:
-                v_p *= random.uniform(0,1)
+                v_p *= random.uniform(0,1)*2
+            elif persSample == 2:
+                v_p = abs(np.random.normal(0, v_p))
 
             if dice1 < 0.5 : #random walk to the right
                 Mposition[i, 0] += v_p + v_d
@@ -143,12 +149,17 @@ def systemLength(RodsMatrix, numberOfMotors):
 
 def multipleSimulations(numberOfSimulations, length, numberOfRods, finalTime, recordTime, plotEvol):
     
+    "Initialise arrays."
     finalLength = np.zeros(numberOfSimulations)
     allLengths = np.zeros((numberOfSimulations, int(finalTime/recordTime)))
+    allMotors = np.zeros((numberOfSimulations, numberOfRods))
     seeds = [None]*numberOfSimulations
+
+    "Create seeds for initial conditions."
     for simul, seed in enumerate(np.random.choice(1000000, numberOfSimulations, replace=False)):
         seeds[simul] = np.random.default_rng(seed=seed)
     
+    "Execute simulations."
     for simul in range(0, numberOfSimulations):
         rods = np.zeros((numberOfRods, 2))
         motors = seeds[simul].random((numberOfRods, 2))
@@ -167,8 +178,25 @@ def multipleSimulations(numberOfSimulations, length, numberOfRods, finalTime, re
 
         finalLength[simul] = lengthArray[-1]
         allLengths[simul] = lengthArray
+        allMotors[simul] = motors[:, 0]
 
-    return finalLength, allLengths
+    return finalLength, allLengths, allMotors
+
+def multipleSimulsSame(type_v, max_v, number, finalTime, recordTime, startFrom, rodsArray, motorsArray):
+    results = [None]*number
+    rods = rodsArray
+    motors = motorsArray
+    for simul in range(0, number):
+        for _ in range(0, int(finalTime/recordTime)):
+            if type_v == 0:
+                rods, motors = randwalk(recordTime, rods, motors, numberOfRods, velocity_p, (max_v/number)*simul)
+            else:
+                rods, motors = randwalk(recordTime, rods, motors, numberOfRods, (max_v/number)*simul, velocity_d)
+        
+        results[simul] = np.divide(np.extract(motors[:, 0] >= startFrom, motors).size, numberOfRods)
+        if simul % 5 == 0:
+            print(f'Currently {simul}/{number}')
+    return results
 
 def plotSystem(RodsMatrix, MotorsMatrix, numberOfRods, gridPoints):
 
@@ -185,7 +213,7 @@ def plotSystem(RodsMatrix, MotorsMatrix, numberOfRods, gridPoints):
         x_m = motorPos[row]
         plt.scatter(x_m, y_m)
 
-    plt.xlabel('x coordinate, motors in dots')
+    plt.xlabel('x coordinate, motors in dots (nanometers)')
     plt.ylabel('Rods, rod 1 is y = 0, next rod is y += 0.2')
     plt.title('1D System')
     plt.pause(0.1)
@@ -194,8 +222,8 @@ def plotLengthEvolution(lengths):
 
     plt.plot(np.linspace(0, lengths.size, lengths.size), lengths)
     plt.title('Rod length evolution')
-    plt.xlabel('Time step')
-    plt.ylabel('Rod length')
+    plt.xlabel('Time step (ns)')
+    plt.ylabel('Rod length (nm)')
     #plt.show()
 
 rods = np.zeros((numberOfRods, 2))
@@ -212,7 +240,7 @@ for tstep in range(0, int(finalTime/recordTime)):
 plt.show()
 plotLengthEvolution(lengthArray)
 if repeatedSystems == True:
-    lengthFinal, lengths = multipleSimulations(numberOfRepetitions, length, numberOfRods, finalTime, recordTime, False)
+    lengthFinal, lengths, motors_all = multipleSimulations(numberOfRepetitions, length, numberOfRods, finalTime, recordTime, False)
 plt.show()
 
 # Save to files, use graphs jupyter notebook to plot these
@@ -221,3 +249,16 @@ np.savetxt('motors.dat', motors)
 if repeatedSystems == True:
     np.savetxt('length_final.dat', lengthFinal)
     np.savetxt('length_all.dat', lengths)
+    np.savetxt('motors_multiple.dat', motors_all)
+
+
+y = multipleSimulsSame(1, 4, 40, finalTime, recordTime, 990, rods, motors)
+x = np.linspace(0.1, 4, 40)
+
+plt.scatter(x, y)
+plt.title('Varying v_d')
+plt.xlabel('v_d')
+plt.ylabel('amount of motors with x > 990')
+plt.show()
+np.savetxt('v_d_change.dat', y)
+
